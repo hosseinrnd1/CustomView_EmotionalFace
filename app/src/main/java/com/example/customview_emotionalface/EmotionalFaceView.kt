4bbbd98acd70c3d552e91b78e036d86291e60c0d
hosseinrnd1 <arzanihossein@gmail.com>
2023-04-19 13:12:36 +0330

    draw faceBackground

---
+++ app/src/main/java/com/example/customview_emotionalface/EmotionalFaceView.kt
@@ -0,0 +1,53 @@
+package com.example.customview_emotionalface
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.util.AttributeSet
+import android.view.View
+
+class EmotionalFaceView(context: Context?, attrs: AttributeSet?) : View(context, attrs) {
+    private val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    private var faceColor = Color.YELLOW
+    private var eyesColor = Color.BLACK
+    private var mouthColor = Color.BLACK
+    private var borderColor = Color.BLACK
+
+    // Face border width in pixels
+    private var borderWidth = 4.0f
+
+    // View size in pixels
+    private var size = 320
+
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+
+        drawFaceBackground(canvas)
+    }
+
+    private fun drawFaceBackground(canvas: Canvas) {
+        paint.color = faceColor
+        paint.style = Paint.Style.FILL
+
+        val radius = size / 2f
+
+        canvas.drawCircle(size / 2f, size / 2f, radius, paint)
+
+        paint.color = borderColor
+        paint.style = Paint.Style.STROKE
+
+        canvas.drawCircle(size / 2f, size / 2f, radius - borderWidth, paint)
+    }
+
+
+    private fun drawEyes(canvas: Canvas?) {
+
+    }
+
+    private fun drawMouth(canvas: Canvas?) {
+
+    }
+
+}
